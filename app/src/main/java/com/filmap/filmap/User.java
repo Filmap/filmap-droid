6bd673ade6f99aec19de039953ba1e8c50d60bd2
Duarte Fernandes <duarte_fq@hotmail.com>
2016-03-12 23:11:30 -0300

    ajustes na tela login e models criados

---
+++ app/src/main/java/com/filmap/filmap/User.java
@@ -0,0 +1,40 @@
+package com.filmap.filmap;
+
+/**
+ * Created by duartemac on 2016-03-12.
+ */
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
